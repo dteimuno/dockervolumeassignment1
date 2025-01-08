6b82c2223d8db0c87d97c12b321152bd3bda0c18
Dennis Tei-Muno <munodennis@gmail.com>
2025-01-08 00:45:03 -0600

    Create assignment.sh

---
+++ assignment.sh
@@ -0,0 +1,14 @@
+#Assignment: named volumes
+
+#Create postgres container with named volume 'psql-data' using version 9.6.1
+docker container run -d --name psql1 -v $(pwd):/var/lib/postgresql/data postgres:9.6.1
+
+#Check container logs
+docker container logs psql1
+
+#stop container
+docker container stop psql1
+
+#Create new postgres container with same named volume using 9.6.2
+docker container run -d --name psql2 -v $(pwd):/var/lib/postgresql/data postgres:9.6.2
+docker container logs psql2
