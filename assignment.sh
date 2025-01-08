e9c66aad09481ae3481d8b2da69102071cff43cb
Dennis Tei-Muno <munodennis@gmail.com>
2025-01-08 00:47:11 -0600

    Update assignment.sh

--- assignment.sh
+++ assignment.sh
@@ -1,7 +1,7 @@
 #Assignment: named volumes
 
 #Create postgres container with named volume 'psql-data' using version 9.6.1
-docker container run -d --name psql1 -v $(pwd):/var/lib/postgresql/data postgres:9.6.1
+docker container run -d --name psql1 -e POSTGRES_PASSWORD=mypassword -v $(pwd):/var/lib/postgresql/data postgres:9.6.1
 
 #Check container logs
 docker container logs psql1
@@ -10,5 +10,5 @@ docker container logs psql1
 docker container stop psql1
 
 #Create new postgres container with same named volume using 9.6.2
-docker container run -d --name psql2 -v $(pwd):/var/lib/postgresql/data postgres:9.6.2
+docker container run -d --name psql2 -e POSTGRES_PASSWORD=mypassword -v $(pwd):/var/lib/postgresql/data postgres:9.6.2
 docker container logs psql2
